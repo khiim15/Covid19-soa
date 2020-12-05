db011d45650ac83a3c1995c022e0919d2568f2cf
Chanikan Piyapong <chanikan152543@gmail.com>
2020-12-05 20:44:11 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,21 @@ const db = require('../db/database');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  
+  const result = await db.getAllCountry();
+  const confirmed = await db.getAllConfirmed();
+  const recovered = await db.getAllRecovered();
+  const death = await db.getAllDeath();
+
+  let objectCountry = [];
+  for (const key in result.rows) {
+    objectCountry[key] = {
+      state: result.rows[key].state,
+      country: result.rows[key].country,
+      confirmed: confirmed.rows[key].confirmed,
+      recovered: recovered.rows[key].confirmed,
+      death: death.rows[key].confirmed,
+    }
+  }
   res.render('index', { countrys: objectCountry });
 });
 
@@ -33,6 +47,25 @@ router.get('/home', async function(req, res, next) {
 
 
 router.get('/chart', async function(req,res,next) {
+  const result = await db.getAllCountry();
+  const confirmed = await db.getAllConfirmed();
+  const recovered = await db.getAllRecovered();
+  const death = await db.getAllDeath();
+
+  let objectCountry = [];
+  for (const key in result.rows) {
+    objectCountry[key] = {
+      state: result.rows[key].state,
+      country: result.rows[key].country,
+      confirmed: confirmed.rows[key].confirmed,
+      recovered: recovered.rows[key].confirmed,
+      death: death.rows[key].confirmed,
+    }
+  }
+
+
+
+
   res.render('chart');
 
 });
