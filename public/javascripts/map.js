d531b674868ae6eedb0f5e6c147e66c4dd1d1bb9
Chanikan Piyapong <chanikan152543@gmail.com>
2020-12-06 15:35:33 +0700

    Update map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,14 +8,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-    for(const key in parseData){
-        L.marker([parseData[key].lat, parseData[key].long]).addTo(map);
-    }
-
 
 
 for (const key in parseData){
     if(parseData[key].value == null){
+        L.circle([parseData[key].lat, parseData[key].long], {
+            color: 'red',
+            fillColor: '#b30000',
+            fillOpacity: 0.5,
+            radius: 200000
+        }).addTo(map);
+
+    }
+    else if(parseData[key].value >= 5000){
         L.circle([parseData[key].lat, parseData[key].long], {
             color: 'red',
             fillColor: '#f03',
@@ -24,24 +29,16 @@ for (const key in parseData){
         }).addTo(map);
 
     }
-    else if(parseData[key].value < 5000 && parseData[key].value <=400 ){
+    else if(parseData[key].value < 5000 && parseData[key].value >= 500){
         L.circle([parseData[key].lat, parseData[key].long], {
-            color: 'yellow',
-            fillColor: '',
-            fillOpacity: 0.5,
-            radius: 200000
-        }).addTo(map);
-
-    }
-    else if(parseData[key].value < 399 ){
-        L.circle([parseData[key].lat, parseData[key].long], {
-            color: 'green',
+            color: 'orenge',
             fillColor: '#f03',
             fillOpacity: 0.5,
             radius: 200000
         }).addTo(map);
 
     }
+    
 }
 }
 
